c78d0a7f92a0793d4838952950205f9d98872421
Hritikpatidar417 <harshupati4@gmail.com>
2024-12-25 00:24:33 +0530

    first c

---
+++ src/main/java/com/parkingallocationyash/Entity/Parking.java
@@ -0,0 +1,9 @@
+package com.parkingallocationyash.Entity;
+
+public class Parking {
+
+    private String slot;
+    private String Status;
+
+
+}
---
+++ src/main/java/com/parkingallocationyash/Entity/ParkingHistory.java
@@ -0,0 +1,41 @@
+package com.parkingallocationyash.Entity;
+
+public class ParkingHistory {
+
+    private String name;
+    private int empId;
+    private String vehicleNo;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
